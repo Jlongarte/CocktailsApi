2ba31140ce2fae391fddc667bed737894ef3627c
Janire <longarte.janire@gmail.com>
2026-06-04 15:17:49 +0200

    Fix total: forzando limpieza de Auth middleware

--- src/middlewares/Auth.middleware.js
+++ src/middlewares/Auth.middleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const User = require("../models/User.model");
 const { verifyToken } = require("../utils/token");
 
@@ -31,37 +30,3 @@ const isAuth = (allowedRoles = []) => {
 };
 
 module.exports = isAuth;
-=======
-const User = require("../models/User.model");
-const { verifyToken } = require("../utils/token");
-
-const isAuth = (allowedRoles = []) => {
-  return async (req, res, next) => {
-    try {
-      const token = req.headers.authorization?.replace("Bearer ", "");
-      if (!token) {
-        return res.status(401).json("Token has not been provided");
-      }
-
-      const decoded = verifyToken(token);
-
-      const user = await User.findById(decoded.id);
-      if (!user) {
-        return res.status(401).json("Ivalid Token or user not found");
-      }
-
-      req.user = user;
-
-      if (allowedRoles.length > 0 && !allowedRoles.includes(user.rol)) {
-        return res.status(403).json("Access denied");
-      }
-
-      next();
-    } catch (error) {
-      return res.status(401).json(error, "Ivalid Token / Session expired");
-    }
-  };
-};
-
-module.exports = isAuth;
->>>>>>> 7f747d20de1779175b98afc5a9c3e386c9a042be
