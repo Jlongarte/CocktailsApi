c3574cbea13234b5f564896923c5e37575b4c09c
Janire <longarte.janire@gmail.com>
2026-06-04 15:24:18 +0200

    Fix definitivo: Purga total de conflictos

--- src/middlewares/files.middleware.js
+++ src/middlewares/files.middleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
@@ -13,19 +12,3 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({ storage });
 module.exports = upload;
-=======
-const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "CocktailsApi",
-    allowFormats: ["jpg", "jpeg", "png", "webp"],
-  },
-});
-
-const upload = multer({ storage });
-module.exports = upload;
->>>>>>> 7f747d20de1779175b98afc5a9c3e386c9a042be
