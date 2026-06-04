6969e04ce5e727a0e5c6b2a8faaa7f37ec860bab
Janire <longarte.janire@gmail.com>
2026-06-04 15:10:57 +0200

    Fix: limpiar de verdad la carpeta utils

--- src/utils/cloudinary.utils.js
+++ src/utils/cloudinary.utils.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
@@ -27,37 +26,3 @@ const deleteImgCloudinary = async (imgId) => {
 };
 
 module.exports = { deleteImgCloudinary, getImageUrl };
-
-//  node .\src\seed\Cocktails.seed.js
-=======
-require("dotenv").config();
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const getImageUrl = (imgId) => {
-  return cloudinary.url(imgId, {
-    secure: true,
-  });
-};
-
-const deleteImgCloudinary = async (imgId) => {
-  if (!imgId) {
-    return;
-  }
-
-  try {
-    await cloudinary.uploader.destroy(imgId);
-  } catch (error) {
-    console.error("Failed to erase de image");
-  }
-};
-
-module.exports = { deleteImgCloudinary, getImageUrl };
-
-//  node .\src\seed\Cocktails.seed.js
->>>>>>> 7f747d20de1779175b98afc5a9c3e386c9a042be
--- src/utils/token.js
+++ src/utils/token.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const jwt = require("jsonwebtoken");
 const JWT = process.env.JWT_SECRET;
 
@@ -11,17 +10,3 @@ const verifyToken = (token) => {
 };
 
 module.exports = { generateToken, verifyToken };
-=======
-const jwt = require("jsonwebtoken");
-const JWT = process.env.JWT_SECRET;
-
-const generateToken = (id, email) => {
-  return jwt.sign({ id, email }, JWT, { expiresIn: "5d" });
-};
-
-const verifyToken = (token) => {
-  return jwt.verify(token, JWT);
-};
-
-module.exports = { generateToken, verifyToken };
->>>>>>> 7f747d20de1779175b98afc5a9c3e386c9a042be
